421188241233ff9c061303249443ad1e0912462d
khoalm4 <khoalm4@vng.com.vn>
2022-01-24 11:52:58 +0700

    abc

--- qdeepl/funcs.py
+++ qdeepl/funcs.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+from gym.envs.classic_control import rendering
+
 
 def process_state_image(state):
     state = cv2.cvtColor(state, cv2.COLOR_BGR2GRAY)
@@ -13,3 +15,12 @@ def generate_state_frame_stack_from_queue(deque):
     #print(frame_stack.shape)
     # Move stack dimension to the channel dimension (stack, x, y) -> (x, y, stack)
     return np.transpose(frame_stack, (1, 2, 0))
+
+def disable_view_window():
+    org_constructor = rendering.Viewer.__init__
+
+    def constructor(self, *args, **kwargs):
+        org_constructor(self, *args, **kwargs)
+        self.window.set_visible(visible=False)
+
+    rendering.Viewer.__init__ = constructor
--- qdeepl/train.py
+++ qdeepl/train.py
@@ -11,9 +11,14 @@ SKIP_FRAMES = 2
 TRAINING_BATCH_SIZE = 64
 SAVE_TRAINING_FREQUENCY = 50
 UPDATE_TARGET_MODEL_FREQUENCY = 10
+from IPython import display
+import matplotlib.pyplot as plt
+%matplotlib inline
 
 if __name__ == '__main__':
+    disable_view_window()
     env = gym.make("CarRacing-v0")
+
     agent = DQNAgent()
 
     for e in range(STARTING_EPISODE, ENDING_EPISODE):
